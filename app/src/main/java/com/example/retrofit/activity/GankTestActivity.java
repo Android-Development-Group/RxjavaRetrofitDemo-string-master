5f8a983b1374506b0a1bd8a844fe5898735d3915
Jusenr <zhouxingrong@putao.com>
2017-04-11 20:11:15 +0800

    fix

---
+++ app/src/main/java/com/example/retrofit/activity/GankTestActivity.java
@@ -0,0 +1,15 @@
+package com.example.retrofit.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.retrofit.R;
+
+public class GankTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gank_test);
+    }
+}
