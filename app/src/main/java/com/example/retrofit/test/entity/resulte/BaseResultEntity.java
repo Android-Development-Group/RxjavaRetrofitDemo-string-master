7b1fd640f1adc07831b58b33195ed1f6ac229052
Jusenr <zhouxingrong@putao.com>
2017-04-12 16:43:56 +0800

    ...

--- app/src/main/java/com/example/retrofit/test/entity/resulte/BaseResultEntity.java
+++ app/src/main/java/com/example/retrofit/test/entity/resulte/BaseResultEntity.java
@@ -2,14 +2,14 @@ package com.example.retrofit.test.entity.resulte;
 
 /**
  * 回调信息统一封装类
- * Created by WZG on 2016/7/16.
+ * Created by Jusenr on 2017/4/12.
  */
-public class BaseResultEntity<T>{
-    //  判断标示
-    private int ret;
-    //    提示信息
+public class BaseResultEntity<T> {
+    //判断标示
+    private int code;
+    //提示信息
     private String msg;
-    //显示数据（用户需要关心的数据）
+    //显示数据（我们需要关心的数据）
     private T data;
 
     public String getMsg() {
@@ -20,13 +20,12 @@ public class BaseResultEntity<T>{
         this.msg = msg;
     }
 
-
-    public int getRet() {
-        return ret;
+    public int getCode() {
+        return code;
     }
 
-    public void setRet(int ret) {
-        this.ret = ret;
+    public void setCode(int code) {
+        this.code = code;
     }
 
     public T getData() {
